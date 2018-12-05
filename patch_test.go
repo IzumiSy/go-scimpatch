0898851fdd3d6ce0c638afc5e4961b15209acb7c
IzumiSy <beetle-noise@gmx.com>
2018-12-05 19:29:30 +0900

    Added unit test for group schema

--- patch_test.go
+++ patch_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestApplyPatch(t *testing.T) {
+func TestApplyPatchUsers(t *testing.T) {
 	schema := &Schema{}
 	err := json.Unmarshal([]byte(UserSchemaJson), &schema)
 	assert.Nil(t, err)
@@ -313,6 +313,69 @@ const TestUserJson = `
     "lastModified": "2016-05-13T04:42:34Z",
     "version": "W\/\"a330bc54f0671c9\"",
     "location": "https://example.com/v2/Users/6B69753B-4E38-444E-8AC6-9D0E4D644D80"
+  }
+}
+`
+
+func TestApplyPatchGroup(t *testing.T) {
+	schema := &Schema{}
+	err := json.Unmarshal([]byte(GroupSchemaJson), &schema)
+	assert.Nil(t, err)
+
+	for _, test := range []struct {
+		patch     Patch
+		assertion func(r *Resource, err error)
+	}{
+		{
+			Patch{Op: "Add", Path: "members", Value: []interface{}{
+				map[string]interface{}{"$ref": "null", "value": "hogehoge_group_id"},
+			}},
+			func(r *Resource, err error) {
+				assert.Nil(t, err)
+				membersVal := reflect.ValueOf(r.GetData()["members"])
+				if membersVal.Kind() == reflect.Interface {
+					membersVal = membersVal.Elem()
+				}
+				assert.Equal(t, 3, membersVal.Len())
+				assert.True(t, reflect.DeepEqual(membersVal.Index(2).Interface(), map[string]interface{}{
+					"$ref": "null", "value": "hogehoge_group_id",
+				}))
+			},
+		},
+	} {
+		data := make(map[string]interface{}, 0)
+		err := json.Unmarshal([]byte(TestGroupJson), &data)
+		assert.Nil(t, err)
+
+		resource := &Resource{Complex(data)}
+		err = ApplyPatch(test.patch, resource, schema)
+		test.assertion(resource, err)
+	}
+}
+
+const TestGroupJson = `
+{
+	"schemas": ["urn:ietf:params:scim:schemas:core:2.0:Group"],
+	"id": "e9e30dba-f08f-4109-8486-d5c6a331660a",
+	"displayName": "Tour Guides",
+	"members": [
+    {
+      "value": "2819c223-7f76-453a-919d-413861904646",
+      "$ref": "https://example.com/v2/Users/2819c223-7f76-453a-919d-413861904646",
+      "display": "Babs Jensen"
+    },
+    {
+      "value": "902c246b-6245-4190-8e05-00816be7344a",
+      "$ref": "https://example.com/v2/Users/902c246b-6245-4190-8e05-00816be7344a",
+      "display": "Mandy Pepperidge"
+    }
+  ],
+  "meta": {
+    "resourceType": "Group",
+    "created": "2010-01-23T04:56:22Z",
+    "lastModified": "2011-05-13T04:42:34Z",
+    "version": "W\/\"3694e05e9dff592\"",
+    "location": "https://example.com/v2/Groups/e9e30dba-f08f-4109-8486-d5c6a331660a"
   }
 }
 `
